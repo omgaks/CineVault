9aba8b3c8e91cff99fff9f491810174d96577937
Ashish Singh <67338965+omgaks@users.noreply.github.com>
2026-06-29 15:47:32 +1300

    Update Color.kt

--- app/src/main/java/com/sole/cinevault/ui/theme/Color.kt
+++ app/src/main/java/com/sole/cinevault/ui/theme/Color.kt
@@ -2,9 +2,11 @@ package com.sole.cinevault.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val CineBackground = Color(0xFF070707)
-val CineCard = Color(0xFF111111)
-val CineGlass = Color(0x661A1A1A)
-val CineAccent = Color(0xFF00C2FF)
-val CineTextPrimary = Color.White
-val CineTextSecondary = Color(0xFFB8B8B8)
+val CineBackground    = Color(0xFF080808)
+val CineCard          = Color(0xFF131313)
+val CineGlass         = Color(0x55181818)
+val CineAccent        = Color(0xFFE8A020)   // warm amber
+val CineAccentDim     = Color(0xFFB07818)
+val CineTextPrimary   = Color.White
+val CineTextSecondary = Color(0xFFAAAAAA)
+val CineSurface       = Color(0xFF1A1A1A)
