e5a86175e5a15a9e73ad5b5ff14e1fc3d489ea0a
Ashish Singh <67338965+omgaks@users.noreply.github.com>
2026-08-03 16:18:28 +1300

    Update PlayerGestureModifiers.kt

--- app/src/main/java/com/sole/cinevault/PlayerGestureModifiers.kt
+++ app/src/main/java/com/sole/cinevault/PlayerGestureModifiers.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChanged
+import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import kotlin.math.abs
