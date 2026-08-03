76325b5e24b9b9f3390f37c4b4d2dc1cdb398649
Ashish Singh <67338965+omgaks@users.noreply.github.com>
2026-08-04 09:49:17 +1300

    Update VideoWithMetadata.kt

--- app/src/main/java/com/sole/cinevault/VideoWithMetadata.kt
+++ app/src/main/java/com/sole/cinevault/VideoWithMetadata.kt
@@ -1,5 +1,7 @@
 package com.sole.cinevault
 
+import com.sole.cinevault.library.VideoFile
+
 import androidx.compose.runtime.Immutable
 
 // Lightweight cast entry stored per video so Actor pages can filter the
