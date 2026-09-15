b57cd8dc0cd28e3875f94d53f3a0064edf025386
Ashish Singh <67338965+omgaks@users.noreply.github.com>
2026-09-16 07:48:01 +1300

    Add files via upload
    Playback resilience 148: clear audio rescue when player session ends

--- app/src/test/java/com/sole/cinevault/AudioRuntimeRescueVideoScopeTest.kt
+++ app/src/test/java/com/sole/cinevault/AudioRuntimeRescueVideoScopeTest.kt
@@ -5,15 +5,23 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class AudioRuntimeRescueVideoScopeTest {
+
     @Test
     fun noActiveRescueIsNoOp() {
         AudioRuntimeRescueController.reset()
-        assertFalse(AudioRuntimeRescueController.resetForVideo("/movies/next.mkv"))
+
+        assertFalse(
+            AudioRuntimeRescueController.resetForVideo("/movies/next.mkv")
+        )
+        assertFalse(
+            AudioRuntimeRescueController.endVideoScope("/movies/next.mkv")
+        )
     }
 
     @Test
     fun resetRestoresPlatformFirst() {
         AudioRuntimeRescueController.reset()
+
         assertTrue(
             AudioRuntimeRescueController.rendererPreference ==
                 CineAudioRendererPreference.PLATFORM_FIRST
