84256ddba1efab45212bd05f8c0081a3a21c8678
Ashish Singh <67338965+omgaks@users.noreply.github.com>
2026-09-18 13:34:55 +1300

    Update PlaybackInfoPanel.kt
    import androidx.compose.foundation.layout.width

--- app/src/main/java/com/sole/cinevault/PlaybackInfoPanel.kt
+++ app/src/main/java/com/sole/cinevault/PlaybackInfoPanel.kt
@@ -1,5 +1,6 @@
 package com.sole.cinevault
 
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
